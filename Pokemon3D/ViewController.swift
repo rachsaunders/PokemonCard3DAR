ae3f9ae6c971cc0056c2029bd78405c4921b41ab
Rach Saunders <rachelsaundersBA@gmail.com>
2019-09-12 18:03:49 +0100

    Card picks up eevee on the nidoran card

--- Pokemon3D/ViewController.swift
+++ Pokemon3D/ViewController.swift
@@ -77,7 +77,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             node .addChildNode(planeNode)
             
-            if let pokeScene = SCNScene(named: "art.scnassets.eevee.scn") {
+            if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn") {
                 
                 if let pokeNode = pokeScene.rootNode.childNodes.first {
                     
